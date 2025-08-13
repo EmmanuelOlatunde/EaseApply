62ae9d14048c716b96fbb0c7f409515389f43174
Olatunde Emmanuel <mohbohlahji@hotmail.com>
2025-08-13 08:49:37 +0100

    deploy sett

--- Frontend/easeapply/vite.config.js
+++ Frontend/easeapply/vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://easeapply.onrender.com',
         changeOrigin: true,
         secure: false,
       }
