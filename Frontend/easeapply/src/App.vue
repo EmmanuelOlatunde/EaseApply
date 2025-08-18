6d20bd593b655bccbf6d46c7d50cfc92d6c81e26
Olatunde Emmanuel <mohbohlahji@hotmail.com>
2025-08-18 10:01:10 +0100

    Update App.vue

--- Frontend/easeapply/src/App.vue
+++ Frontend/easeapply/src/App.vue
@@ -7,11 +7,9 @@
 <script setup>
 import { onMounted, watch } from 'vue'
 import { useAuthStore } from './stores/auth'
-import { useRoute } from 'vue-router'
-import { showToast } from './api'
 
 const authStore = useAuthStore()
-const route = useRoute()
+
 
 onMounted(() => {
   if (authStore.token) {
@@ -21,21 +19,4 @@ onMounted(() => {
   checkStatus(route.query.status)
 })
 
-watch(
-  () => route.query.status,
-  (newStatus) => {
-    checkStatus(newStatus)
-  },
-  { immediate: true }
-)
-
-function checkStatus(status) {
-  if (status === 'success') {
-    showToast('✅ Email verified successfully!')
-  } else if (status === 'invalid') {
-    showToast('❌ Invalid verification link.', 'error')
-  } else if (status === 'expired') {
-    showToast('⚠️ Verification link expired.', 'warning')
-  }
-}
 </script>
